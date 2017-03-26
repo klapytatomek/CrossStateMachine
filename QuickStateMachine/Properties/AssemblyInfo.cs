47dfd7165b25a65cf1f5574f95f5b8f9d94b9d15
tomasz.klapyta <tomasz.klapyta@insanelab.com>
2017-03-26 11:49:23 +0200

    initial nupkg release

--- QuickStateMachine/Properties/AssemblyInfo.cs
+++ QuickStateMachine/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QuickStateMachine")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Quick State Machine for handling states with dedicated class, simple and elegant.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Tomasz Kłapyta")]
 [assembly: AssemblyProduct("QuickStateMachine")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
